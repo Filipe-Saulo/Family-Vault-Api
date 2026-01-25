93e34bec93b5cad6776437b2ac3c0af4e856f59e
Filipe <filipeunithing@gmail.com>
2026-01-25 19:18:13 -0300

    first commit

---
+++ family-vault-api/Program.cs
@@ -0,0 +1,15 @@
+namespace family_vault_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
